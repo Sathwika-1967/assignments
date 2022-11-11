a7b27551a4db82c30f775b0469deb1c4617f3cac
Sathwika-1967 <115360293+Sathwika-1967@users.noreply.github.com>
2022-11-11 04:00:12 +0000

    Add files via upload

---
+++ Assign4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Assign1
+    {
+        internal class StackException
+        {
+            public void Elements()
+            {
+                try
+                {
+                    Stack st = new Stack(6);
+                    Console.WriteLine("---Stack Elements are-----");
+                    st.Push(4);
+                    st.Push(5);
+                    st.Push(6);
+                    st.Push(7);
+                    st.Push(8);
+                    st.Push(9);
+                    st.Push(10);
+                                        
+                    Console.WriteLine("------- Removed Eements are ------");
+                    st.Pop();
+               st.Pop();
+                 
+                    Console.WriteLine("------After Pop Method------");
+          }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            public static void Main()
+            {
+                StackException stackException = new StackException();
+                stackException.Elements();
+            }
+
+
+
+        }
+    }
+
+}
+
---
+++ assign1Q4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign1
+{
+    internal class assignQ4
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+           
+            Console.WriteLine("enter first integer");
+           
+            num1 = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine("enter second integer");
+            
+            num2 = int.Parse(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("After Swapping : ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\n First integer : " + num1);
+            Console.Write("\n Second integer : " + num2);
+            Console.Read();
+
+
+
+
+        }
+    }
+}
+
+    
+
